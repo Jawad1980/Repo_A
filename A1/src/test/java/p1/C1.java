78c8534930af7a1eaa343c96c1c12f4f1b7ead99
jawad <jawad@10.0.0.28>
2021-12-18 15:57:36 -0500

    test A1 is committed

---
+++ A1/src/test/java/p1/C1.java
@@ -0,0 +1,42 @@
+package p1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class C1 {
+	
+	@Test
+	public void tm1() {
+		
+		
+		Reporter.log("=================== test A5 started ==============", true);
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		Reporter.log("=================== browser started ==============", true);
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Reporter.log("=================== window maximized ==============", true);
+		
+		driver.get("https://www.facebook.com/");
+		Reporter.log("=================== facebook opened ==============", true);
+		
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("abcd@yahoo.com");
+		Reporter.log("=================== user name entered ==============", true);
+		
+		driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("demo12345");
+		Reporter.log("=================== password entered ==============", true);
+		
+		Reporter.log("==============test A5 finished =====================", true);
+		
+	}
+
+}
